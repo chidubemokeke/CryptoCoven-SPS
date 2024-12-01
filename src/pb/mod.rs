577a28e039482432ba5304ca1a5ceec8f5153ab8
ChidubemOkeke.eth <christianokeke92@gmail.com>
2024-12-01 04:21:25 +0100

    map-transfers

--- src/pb/mod.rs
+++ src/pb/mod.rs
@@ -24,3 +24,12 @@ pub mod sf {
         }
     }
 }
+pub mod substreams {
+    pub mod entity {
+        // @@protoc_insertion_point(attribute:substreams.entity.v1)
+        pub mod v1 {
+            include!("substreams.entity.v1.rs");
+            // @@protoc_insertion_point(substreams.entity.v1)
+        }
+    }
+}
